0bb8b601a8eb723f23cff346d633ce353f80e4ef
tulolo287 <iv1055@yandex.ru>
2025-02-08 22:43:44 +0300

    add format

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -8,5 +8,6 @@ def gendiff():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
 
     args = parser.parse_args()
