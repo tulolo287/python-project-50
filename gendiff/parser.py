c613f53814b596712ba74f6d5f41ee235f8c1f7c
tulolo287 <iv1055@yandex.ru>
2025-02-20 01:02:52 +0300

    add gen_ast

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -9,7 +9,8 @@ def generate_diff(file_path1, file_path2):
     file1 = load_file(file_path1)
     file2 = load_file(file_path2)
 
-    return formatter(file1, file2)
+    ast = gen_ast(file1, file2)
+    print(formatter(ast))
 
 
 def load_file(file_path):
@@ -20,11 +21,7 @@ def load_file(file_path):
         return yaml.load(open(file_path), Loader=yaml.Loader)
 
 
-def formatter(file1, file2, res=["{"], sp=""):
-    ind = "    "
-    minus_ind = "- "
-    plus_ind = "+ "
-
+def gen_ast(file1, file2, ast=[]):
     file1_elements = set(file1)
     file2_elements = set(file2)
 
@@ -34,6 +31,7 @@ def formatter(file1, file2, res=["{"], sp=""):
     all_data = sorted(file1_elements | file2_elements)
 
     for key in all_data:
+
         if key in file1 and type(file1[key]) is bool:
             file1[key] = str(file1[key]).lower()
         if key in file2 and type(file2[key]) is bool:
@@ -44,66 +42,78 @@ def formatter(file1, file2, res=["{"], sp=""):
         if key in file2 and file2[key] is None:
             file2[key] = "null"
 
-        if key in minus_data:
-            if isinstance(file1[key], dict):
-                res.append(f"{sp}{minus_ind}{key}: {{")
-                tmp = sp
-                sp += ind
-                formatter(file1[key], file1[key], res, sp)
-                sp = tmp
-                res.append(f"{sp}  }}")
-                continue
-            else:
-                res.append(f"{sp}{minus_ind}{key}: {file1[key]}")
-        if key in plus_data:
-            if isinstance(file2[key], dict):
-                res.append(f"{sp}{plus_ind}{key}: {{")
-                tmp = sp
-                sp += ind
-                formatter(file2[key], file2[key], res, sp)
-                sp = tmp
-                res.append(f"{sp}  }}")
-                continue
-            else:
-                res.append(f"{sp}{plus_ind}{key}: {file2[key]}")
-        if key in common_data:
-            if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-                res.append(f"{sp}  {key}: {{")
-                tmp = sp
-                sp += ind
-                formatter(file1[key], file2[key], res, sp)
-                sp = tmp
-                res.append(f"{sp}  }}")
-                continue
-            if file1[key] == file2[key]:
-                res.append(f"{sp}  {key}: {file1[key]}")
-            else:
-                if isinstance(file1[key], dict):
-                    res.append(f"{sp}{minus_ind}{key}: {{")
-                    tmp = sp
-                    sp += ind
-                    formatter(file1[key], file1[key], res, sp)
-                    sp = tmp
-                    res.append(f"{sp}  }}")
-                    res.append(f"{sp}{plus_ind}{key}: {file2[key]}")
-                elif isinstance(file2[key], dict):
-                    res.append(f"{sp}{plus_ind}{key}: {{")
-                    tmp = sp
-                    sp += ind
-                    formatter(file2[key], file2[key], res, sp)
-                    sp = tmp
-                    res.append(f"{sp}  }}")
-                    res.append(f"{sp}{minus_ind}{key}: {file1[key]}")
-                else:
-                    if not file1[key]:
-                        res.append(f"{sp}{minus_ind}{key}:")
-                        res.append(f"{sp}{plus_ind}{key}: {file2[key]}")
-                    elif not file2[key]:
-                        res.append(f"{sp}{minus_ind}{key}: {file1[key]}")
-                        res.append(f"{sp}{plus_ind}{key}:")
-                    else:
-                        res.append(f"{sp}{minus_ind}{key}: {file1[key]}")
-                        res.append(f"{sp}{plus_ind}{key}: {file2[key]}")
+        if (
+            key in file1
+            and isinstance(file1[key], dict)
+            and key in file2
+            and isinstance(file2[key], dict)
+        ):
+            node = gen_ast(file1[key], file2[key])
+            ast.append(
+                {
+                    "name": key,
+                    "node_type": "list",
+                    "value": node,
+                    "action": "unchanged",
+                }
+            )
 
-    res = "\n  ".join(res) + "\n}\n"
+        elif key in common_data:
+            ast.append(
+                {
+                    "name": key,
+                    "node_type": "node",
+                    "value": file1[key],
+                    "action": "unchanged",
+                }
+            )
+
+        elif key in minus_data:
+            if isinstance(file1[key], dict):
+                gen_ast(file1[key], file1[key])
+            else:
+                ast.append(
+                    {
+                        "name": key,
+                        "node_type": "node",
+                        "value": file1[key],
+                        "action": "remove",
+                    }
+                )
+        elif key in plus_data:
+            if isinstance(file2[key], dict):
+                gen_ast(file2[key], file2[key])
+            else:
+                ast.append(
+                    {
+                        "name": key,
+                        "node_type": "node",
+                        "value": file2[key],
+                        "action": "add",
+                    }
+                )
+
+    return ast
+
+
+def formatter(ast, res=[]):
+    for item in ast:
+        name = item["name"]
+        action = item["action"]
+        value = item["value"]
+        node_type = item["node_type"]
+        match action:
+            case "remove":
+                sign = "- "
+            case "add":
+                sign = "+ "
+            case "unchanged":
+                sign = "  "
+
+        if node_type == "node":
+            res.append(f"{sign}{name}: {value}")
+        else:
+            formatter(ast)
+        print(name)
+    "\n".join(res)
     return res
